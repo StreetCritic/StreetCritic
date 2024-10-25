18ecaf063fc97e05fddf3fbf77a1c2122d0eace7
Christian Neumann <cneumann@e3code.de>
2024-10-25 14:40:12 +0200

    Rename rating column of rated_segments view.

--- database/views/rated_segments.sql
+++ database/views/rated_segments.sql
@@ -3,7 +3,7 @@
 CREATE OR REPLACE VIEW rated_segments AS
   SELECT
     way_segment.segment_id,
-    AVG(intervals._rating)
+    AVG(intervals._rating) AS rating
     FROM way_segment
          LEFT JOIN segment ON way_segment.segment_id = segment.id
          INNER JOIN get_stops_for_segment(way_segment.segment_id) AS intervals ON true
