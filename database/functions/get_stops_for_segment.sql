257e8735867aab2e7a6e7420dbbc87b82ed9b481
Christian Neumann <cneumann@e3code.de>
2024-12-10 12:08:39 +0100

    Fix get_stops_for_segment SQL function.

--- database/functions/get_stops_for_segment.sql
+++ database/functions/get_stops_for_segment.sql
@@ -1,4 +1,9 @@
 -- For the given segment, return all rated intervals.
+--
+-- There may be many (rated) ways that span the segment. The segment is divided
+-- in interval that are created by the endpoints of the ways.
+-- Then, for each such interval, all rated ways are considered and their ratings
+-- averaged and applied to the interval. These rated intervals are returned.
 CREATE OR REPLACE FUNCTION get_stops_for_segment(_the_segment CHAR(32))
   RETURNS TABLE (_start DOUBLE PRECISION, _stop DOUBLE PRECISION, _rating NUMERIC)
   LANGUAGE PLPGSQL
@@ -7,9 +12,12 @@ BEGIN
   RETURN QUERY
     WITH RECURSIVE
 
-    -- Get all way segments, ignore the ones without rating.
+    -- Get all rated way (sub-)segments, ignore the ones without rating.
+    -- Averages the rating for same start and stop.
     start_stops AS (
-      SELECT start, stop, AVG(way_rating.general_rating) AS rating
+      SELECT LEAST(start, stop) AS start,
+             GREATEST(start, stop) AS stop,
+             AVG(way_rating.general_rating) AS rating
         FROM way_segment LEFT JOIN way_rating
                              ON way_segment.way_id=way_rating.way_id
        WHERE segment_id=_the_segment AND way_rating.general_rating IS NOT NULL
