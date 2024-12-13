f9e17a66858981259b83de1276244162c5b54246
Christian Neumann <cneumann@e3code.de>
2024-12-13 14:39:11 +0100

    Add more social media links.

--- frontend/src/components/header/SocialMediaLinks.tsx
+++ frontend/src/components/header/SocialMediaLinks.tsx
@@ -1,5 +1,12 @@
 import { Group } from "@mantine/core";
-import { GithubLogo, MastodonLogo } from "@phosphor-icons/react";
+import {
+  GithubLogo,
+  InstagramLogo,
+  LinkedinLogo,
+  MastodonLogo,
+  ThreadsLogo,
+  XLogo,
+} from "@phosphor-icons/react";
 
 import classes from "./SocialMediaLinks.module.css";
 
@@ -11,10 +18,22 @@ export default function SocialMediaLinks({ visibleFrom }: Props) {
   return (
     <Group h="100%" gap={10} className={classes.root} visibleFrom={visibleFrom}>
       <a href="https://digitalcourage.social/@streetcritic" target="_blank">
-        <MastodonLogo size={32} />
+        <MastodonLogo size={24} />
+      </a>
+      <a href="https://x.com/StreetCriticOrg" target="_blank">
+        <XLogo size={24} />
       </a>
       <a href="https://github.com/streetcritic" target="_blank">
-        <GithubLogo size={32} />
+        <GithubLogo size={24} />
+      </a>
+      <a href="https://www.linkedin.com/company/streetcritic" target="_blank">
+        <LinkedinLogo size={24} />
+      </a>
+      <a href="https://instagram.com/streetcritic" target="_blank">
+        <InstagramLogo size={24} />
+      </a>
+      <a href="https://www.threads.net/@streetcritic" target="_blank">
+        <ThreadsLogo size={24} />
       </a>
     </Group>
   );
