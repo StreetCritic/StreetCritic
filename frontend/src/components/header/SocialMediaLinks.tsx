b7f22dbf30aa08f7e76e18c3c8457a654d31929e
Christian Neumann <cneumann@e3code.de>
2025-01-09 20:49:09 +0100

    Removed some social media links.

--- frontend/src/components/header/SocialMediaLinks.tsx
+++ frontend/src/components/header/SocialMediaLinks.tsx
@@ -20,20 +20,11 @@ export default function SocialMediaLinks({ visibleFrom }: Props) {
       <a href="https://digitalcourage.social/@streetcritic" target="_blank">
         <MastodonLogo size={24} />
       </a>
-      <a href="https://x.com/StreetCriticOrg" target="_blank">
-        <XLogo size={24} />
-      </a>
-      <a href="https://github.com/streetcritic" target="_blank">
-        <GithubLogo size={24} />
-      </a>
-      <a href="https://www.linkedin.com/company/streetcritic" target="_blank">
-        <LinkedinLogo size={24} />
-      </a>
       <a href="https://instagram.com/streetcritic" target="_blank">
         <InstagramLogo size={24} />
       </a>
-      <a href="https://www.threads.net/@streetcritic" target="_blank">
-        <ThreadsLogo size={24} />
+      <a href="https://github.com/streetcritic" target="_blank">
+        <GithubLogo size={24} />
       </a>
     </Group>
   );
