4d22e7dc9b68d7bdf88c2c0c2f5be10ffbd956db
Christian Neumann <cneumann@e3code.de>
2025-02-13 15:45:50 +0100

    Rework way colors.

--- frontend/src/components/map/colors.ts
+++ frontend/src/components/map/colors.ts
@@ -4,17 +4,20 @@ import chroma from "chroma-js";
  * Returns the scale for the rating colors.
  */
 export function ratingScale() {
-  return chroma.scale([
-    "fff5eb",
-    "fee6ce",
-    "fdd0a2",
-    "fdae6b",
-    "fd8d3c",
-    "f16913",
-    "d94801",
-    "a63603",
-    "7f2704",
-  ]);
+  return chroma
+    .scale([
+      "fff5eb",
+      "fee6ce",
+      "fdd0a2",
+      "fdae6b",
+      "fd8d3c",
+      "f16913",
+      "d94801",
+      "a63603",
+      "7f2704",
+    ])
+    .gamma(1.5)
+    .classes(5);
 }
 
 /**
