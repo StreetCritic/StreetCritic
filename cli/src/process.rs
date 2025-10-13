255b13fedebe31f5db97b364ab0d073b54890820
Christian Neumann <cneumann@e3code.de>
2025-10-13 15:39:31 +0200

    Revert changes that should not be part of earlier commit.

--- cli/src/process.rs
+++ cli/src/process.rs
@@ -1,9 +1,8 @@
-use std::sync::{Arc, atomic::{AtomicUsize, Ordering}, Mutex};
 use std::{path::Path, time::Instant};
 
 use anyhow;
 
-use osm_io::osm::{model::{element::Element, tag::Tag, way::Way}, pbf::thread_local_accumulator::ThreadLocalAccumulator};
+use osm_io::osm::model::{element::Element, tag::Tag, way::Way};
 use osm_io::osm::pbf;
 use osm_io::osm::pbf::compression_type::CompressionType;
 use osm_io::osm::pbf::file_info::FileInfo;
@@ -33,51 +32,39 @@ pub fn process(opts: &Options) -> Result<(), anyhow::Error> {
         Some(get_rated_ways(
             opts.db_conn
                 .as_ref()
-                .expect("Missing database connection string")
+                .expect("Missing DB connection string")
                 .as_str(),
         )?)
     } else {
         None
     };
 
-    log::info!("Processing PBF...");
+    log::info!("Started pbf io pipeline");
 
     let reader = pbf::reader::Reader::new(opts.in_path)?;
     let mut file_info = FileInfo::default();
     file_info.with_writingprogram_str("streetcritic-rating");
-    let writer = Arc::new(Mutex::new(pbf::parallel_writer::ParallelWriter::from_file_info(
-        4 * 8000 * 32,
-        8000,
+    let mut writer = pbf::writer::Writer::from_file_info(
         opts.out_path.to_path_buf(),
         file_info,
         CompressionType::Zlib,
-    )?));
+    )?;
 
-    let writer_clone = writer.clone();
+    writer.write_header()?;
 
-    let tl_acc = ThreadLocalAccumulator::new(8000);
-    {
-        let mut writer_guard = writer.lock().unwrap();
-        writer_guard.write_header()?;
-    }
-
-
-    let count_elements = Arc::new(AtomicUsize::new(0));
-    let count_elements_clone = count_elements.clone();
+    let mut count_elements: u64 = 0;
     let time_pipeline_start = Instant::now();
     let mut last_log = Instant::now();
 
-    let calculate_scores = opts.calculate_scores;
-    let merge_ratings = opts.merge_ratings;
-
-    reader.parallel_for_each(num_cpus::get(), move |element| {
+    // reader.parallel_for_each(num_cpus::get(), move |element| {
+    for element in reader.elements()? {
         if let Element::Way { way } = &element {
             log::trace!("Processing way {}", way.id());
             let mut tags = vec![];
             for tag in way.tags() {
                 tags.push(tag.clone());
             }
-            if merge_ratings && rated_ways.as_ref().unwrap().contains_key(&way.id()) {
+            if opts.merge_ratings && rated_ways.as_ref().unwrap().contains_key(&way.id()) {
                 log::trace!("Way {} has general rating", way.id());
                 tags.push(Tag::new(
                     "streetcritic:rating:general".into(),
@@ -89,7 +76,7 @@ pub fn process(opts: &Options) -> Result<(), anyhow::Error> {
                         .to_string(),
                 ));
             }
-            if calculate_scores {
+            if opts.calculate_scores {
                 let indicators = indicators::calculate(way);
                 for (key, value) in indicators {
                     log::trace!("indicator {}: {}", key, value);
@@ -99,7 +86,7 @@ pub fn process(opts: &Options) -> Result<(), anyhow::Error> {
                     ));
                 }
             }
-            tl_acc.add(Element::Way {
+            writer.write_element(Element::Way {
                 way: Way::new(
                     way.id(),
                     // TODO maybe honor these. We're doing an update here!
@@ -111,27 +98,23 @@ pub fn process(opts: &Options) -> Result<(), anyhow::Error> {
                     way.visible(),
                     way.refs().clone(),
                     tags,
-                )});
-        } else if element.is_sentinel() {
-            let mut writer_guard = writer_clone.lock().unwrap();
-            writer_guard.write_elements(tl_acc.elements())?;
-        } else {
-            tl_acc.add(element);
+                ),
+            })?;
+            continue;
         }
-        count_elements_clone.fetch_add(1, Ordering::Relaxed);
-        // if last_log.elapsed().as_secs() >= 10 {
-        //     log::info!("Read {} elements", count_elements);
-        //     last_log = Instant::now();
-        // }
-        Ok(())
-    })?;
+        writer.write_element(element)?;
+        count_elements += 1;
+        if last_log.elapsed().as_secs() >= 10 {
+            log::info!("Read {} elements", count_elements);
+            last_log = Instant::now();
+        }
+    }
 
-    writer.lock().unwrap().close()?;
+    writer.close()?;
 
     log::info!(
-        "PBF completely read, elements: {}, time: {} ms",
-        count_elements.load(Ordering::SeqCst),
-        time_pipeline_start.elapsed().as_millis()
+        "Finished pbf io pipeline, time: {:?}",
+        time_pipeline_start.elapsed().as_secs()
     );
     Ok(())
 }
